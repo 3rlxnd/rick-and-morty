5d8c1357fe04658b81ae4daf255476c39c7f44a3
3rlxnd <erlnd.gdt@gmail.com>
2025-01-13 15:25:04 +0100

    Solved

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -1,5 +1,6 @@
 export default function NavButton(type) {
     const button = document.createElement('button');
+    button.classList.add('button')
     button.textContent = type
     button.setAttribute('data-js', `button-${type}`)
     return button
